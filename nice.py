4332beaecea9a5cec4959ae2c317e69b8066d3b4
Davood <75040047+davoodmdibh@users.noreply.github.com>
2020-11-25 22:20:57 +0330

    Create nice.py

---
+++ nice.py
@@ -0,0 +1,9 @@
+ask_username = input("Please Enter you Username: ")
+list_username = ['davoodmohammadi','sara','mohammad','ali','jack']
+if (ask_username == list_username):
+  print(ask_password)
+ask_password = input("Please enter you password: ")
+list_password = ['davood123','sarasadehgi1','davood!@#DIBH123','123abc','jacknamepassword1234']
+if (ask_password == list_password):
+  print("thanks a lot for long in☺")
+input("Please press enter.......")
